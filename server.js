3f7fad275e956204138b87a66a83a8dd0c33241e
swetharohith <swetharohith20@gmail.com>
2016-09-19 16:09:02 +1000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/aricle-one',function(req,res){
+app.get('/article-one',function(req,res){
     res.send("rticle one requested and will be served here");
 });
 app.get('/article-two',function(req,res){
